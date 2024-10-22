4e41ac024485a6f848ea706e43e4ee77c33ae601
whoiscc <correctizer@gmail.com>
2024-10-22 14:40:32 +0800

    Add basic items

---
+++ src/msc/items.py
@@ -0,0 +1,95 @@
+from contextlib import contextmanager
+
+
+class Function:
+    def __init__(self, identifier):
+        self.id = identifier
+        self.params = []
+        self.block = Block()
+
+    def declare_param(self, var):
+        self.params.append(var)
+
+    # utils
+    def __iadd__(self, var):
+        self.declare_param(var)
+        return self
+
+
+class Block:
+    def __init__(self):
+        self.stmts = []
+        self.vars = []
+
+    def declare(self, var):
+        self.vars.append(var)
+        self.stmts.append(Declare(var))
+
+    def append(self, stmt):
+        self.stmts.append(stmt)
+
+    # utils
+    def __ilshift__(self, stmt):
+        self.append(stmt)
+        return self
+
+    def __iadd__(self, var):
+        self.declare(var)
+        return self
+
+    @contextmanager
+    def loop(self, check):
+        block = Block()
+        try:
+            yield block
+        finally:
+            self.append(Loop(check, block))
+
+    def cases(self):
+        return Cases(self)
+
+
+class Cases:
+    def __init__(self, block):
+        self.block = block
+        self.branch = None
+
+    @contextmanager
+    def when(self, check):
+        if self.branch is not None:
+            self.block = self.branch.neg
+            self.branch = None
+        branch = Branch(check, Block(), Block())
+        try:
+            yield branch.pos
+        finally:
+            self.branch = branch
+            self.block.append(branch)
+
+    @contextmanager
+    def otherwise(self):
+        assert self.branch is not None
+        yield self.branch.neg
+
+
+class Variable:
+    def __init__(self, ty):
+        self.ty = ty
+
+
+class Declare:
+    def __init__(self, var):
+        self.var = var
+
+
+class Loop:
+    def __init__(self, check, block):
+        self.check = check
+        self.block = block
+
+
+class Branch:
+    def __init__(self, check, pos, neg):
+        self.check = check
+        self.pos = pos
+        self.neg = neg
---
+++ tests/test_items.py
@@ -0,0 +1,64 @@
+from msc.items import Function
+
+
+def test_empty_function():
+    f = Function("f")
+    assert f.block.stmts == []
+
+
+def test_loop():
+    f = Function("f")
+    with f.block.loop("loop check") as b:
+        b <<= "loop stmt"
+    assert len(f.block.stmts) == 1
+    stmt = f.block.stmts[0]
+    assert stmt.check == "loop check"
+    assert stmt.block.stmts == ["loop stmt"]
+
+
+def test_if():
+    f = Function("f")
+    with f.block.cases().when("if check") as b:
+        b <<= "if stmt"
+    assert len(f.block.stmts) == 1
+    stmt = f.block.stmts[0]
+    assert stmt.check == "if check"
+    assert stmt.pos.stmts == ["if stmt"]
+    assert stmt.neg.stmts == []
+
+
+def test_cases():
+    f = Function("f")
+    cases = f.block.cases()
+    with cases.when("if check") as b:
+        b <<= "if stmt"
+    with cases.when("else if check") as b:
+        b <<= "else if stmt"
+    with cases.otherwise() as b:
+        b <<= "else stmt"
+    assert len(f.block.stmts) == 1
+    stmt = f.block.stmts[0]
+    assert stmt.check == "if check"
+    assert stmt.pos.stmts == ["if stmt"]
+    assert len(stmt.neg.stmts) == 1
+    stmt = stmt.neg.stmts[0]
+    assert stmt.check == "else if check"
+    assert stmt.pos.stmts == ["else if stmt"]
+    assert stmt.neg.stmts == ["else stmt"]
+
+
+def test_multiple_cases():
+    f = Function("f")
+    with f.block.cases().when("if check") as b:
+        b <<= "if stmt"
+    with f.block.cases().when("another if check") as b:
+        b <<= "another if stmt"
+    assert len(f.block.stmts) == 2
+    stmt = f.block.stmts[0]
+    assert stmt.check == "if check"
+    assert stmt.pos.stmts == ["if stmt"]
+    assert stmt.neg.stmts == []
+    stmt = f.block.stmts[1]
+    assert stmt.check == "another if check"
+    assert stmt.pos.stmts == ["another if stmt"]
+    assert stmt.neg.stmts == []
